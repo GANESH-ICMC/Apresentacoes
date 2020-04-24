f1aa15351399c4e55f392b2bd46616d6c982ed56
Vinicius Borges <viniciusborges22@gmail.com>
2020-04-24 15:48:45 -0300

    Melhorias no script de frequencias

--- Aulas/Frentes/Criptografia/Cifras de substituição/frequencia.py
+++ Aulas/Frentes/Criptografia/Cifras de substituição/frequencia.py
@@ -1,14 +1,11 @@
-import operator
+from collections import defaultdict
 
 def calculaFrequencias(mensagem, tamBloco):
-    frequencia = {}
-    for i in range(len(mensagem)-tamBloco+1):
-        if ' ' not in (mensagem[i:i+tamBloco]):
-            bloco = mensagem[i:i+tamBloco]
-            if bloco in frequencia:
-                frequencia[bloco] += 1
-            else:
-                frequencia[bloco] = 1
+    frequencia = defaultdict(int)
+    blocos = (mensagem[i:i+tamBloco] for i in range(len(mensagem) - tamBloco + 1))
+    for bloco in blocos:
+        if ' ' not in bloco:
+            frequencia[bloco] += 1
     return frequencia
 
 # mensagem que se deseja analisar a frequência
@@ -17,12 +14,12 @@ mensagem = ""
 frequencias = list()
 
 # obtem as frequências de blocos de tamanho 2 até 4, inclusos
-for i in range(2, 5):
-    f = calculaFrequencias(mensagem, i).items()
-    frequencias += list(f)
+for tamBloco in range(2, 5):
+    f = calculaFrequencias(mensagem, tamBloco).items()
+    frequencias += f
 
 # ordena pelo valor da frequência
-freq_ord = sorted(frequencias, key=operator.itemgetter(1), reverse=True)
+freq_ord = sorted(frequencias, key=lambda item: item[1], reverse=True)
 
 # limite de frequências abaixo da qual elas não serão mostradas na tela
 limite = 5
